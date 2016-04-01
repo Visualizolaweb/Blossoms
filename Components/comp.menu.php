da049141811c863c07ef9bae0cfe7a3a86f01c22
guillevalen <guivalen@gmail.com>
2016-03-31 22:43:03 -0500

    Inicio del Proyecto

---
+++ Components/comp.menu.php
@@ -0,0 +1,16 @@
+<nav>
+  <div class="nav-wrapper">
+    <div class="row">
+      <div class="col m10 offset-m1">
+        <a href="#" class="brand-logo"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Products & Services</a></li>
+          <li><a href="#">Workshops</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Magdalena Belfort | Licensed Esthetician Providing Facial & Waxing Services</title>
+
+    <link type="text/css" rel="stylesheet" href="Assets/Plugins/Materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="Assets/Css/main.css" />
+  </head>
+  <body>
+    <div class="container-fluid">
+      <?php include("Components/comp.menu.php"); ?>
+    </div>
+  </body>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="Assets/Plugins/Materialize/js/materialize.min.js"></script>
+</html>
